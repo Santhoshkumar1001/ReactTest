e0bb770f774f5f45565c903eb83e1c35e8b7bc0c
Santhoshkumar1001 <123051308+Santhoshkumar1001@users.noreply.github.com>
2023-03-24 19:16:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var count = 0;
+
+  const counter = document.getElementById("counter-value")
+
+  function handleChange(type){
+    if(type === 'decrement' && count >= 1) count = count - 1;
+    else if(type ==='increment') count = count + 1;
+    changeUI(count);
+    
+    }
+    
+    function changeUI(value = 0) {
+      counter.innerText = 1;
+     }   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row" >
+          <div className="counter-container">
+<button className="counter-button"onClick={() => handleChange('decrement')}>-</button>
+<div style={{width:20}}></div>
+<p id = "counter-value" className="counter-text">{count}</p>
+
+<div style={{width:20}}></div>
+<button className="counter-button" onClick={() => handleChange('increment')}>+</button>
+</div>
+
+          </div>
+
+        </div>
+
+      </section>
+      
     </div>
   );
 }
